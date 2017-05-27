3f0f5eef8444730ff2402d3dfde538dab65a95c6
Hector Flores <hecto932@gmail.com>
2017-05-27 14:46:25 -0500

    Configurando la API de Lastfm en Vue.js

---
+++ src/api/config.js
@@ -0,0 +1,11 @@
+export default {
+  appName: 'platzimusic',
+  apiKey: 'd2a5915e2db4cd9da286253c050a7d00',
+  secret: '030171245d09b08ae78fbd04e06e2b73',
+  registeredTo: 'hecto932'
+}
+
+/*Application name  platzimusic
+API key d2a5915e2db4cd9da286253c050a7d00
+Shared secret 030171245d09b08ae78fbd04e06e2b73
+Registered to hecto932*/
